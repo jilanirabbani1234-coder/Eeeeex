73a92e679c95dfb2a5ad64ba067eae8ddefb8c5b
jilanirabbani1234-coder <jilanirabbani1234@gmail.com>
2026-05-04 02:58:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from threading import Thread
 from pyrogram import Client, idle
 
-# Flask Setup (Render Health Check ke liye)
+# Flask for Render
 app = Flask(__name__)
 @app.route('/')
 def status(): return "Bot is Alive! 🚀"
@@ -13,25 +13,23 @@ def run_flask():
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
-# Background mein Flask chalu karein
 Thread(target=run_flask, daemon=True).start()
 
-# Client Setup
+# Pyrogram Client
 bot = Client(
     "my_bot",
     api_id=os.getenv("API_ID"),
     api_hash=os.getenv("API_HASH"),
     bot_token=os.getenv("BOT_TOKEN"),
-    plugins=dict(root="Extractor/modules") # Folder name check kar lena
+    plugins=dict(root="Extractor/modules")
 )
 
 async def main():
     await bot.start()
-    print("Bot Started Successfully!")
+    print("Bot Started!")
     await idle()
 
 if __name__ == "__main__":
-    # Naya event loop create karke bot run karna
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     
