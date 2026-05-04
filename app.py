cf4f9a48bcda740b15dbd6fa8dab9cb3f396f7ab
jilanirabbani1234-coder <jilanirabbani1234@gmail.com>
2026-05-05 02:37:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from threading import Thread
 from pyrogram import Client
 import asyncio
-from utils.cleanup import start_cleanup_scheduler
+#from utils.cleanup import start_cleanup_scheduler
 from myutils.cleanup import start_cleanup_scheduler
 
 # Start the cleanup scheduler
