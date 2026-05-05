093fb96f780e06fffc4f89b5e963474ee5626dd7
jilanirabbani1234-coder <jilanirabbani1234@gmail.com>
2026-05-05 19:17:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "29426008"))
+API_ID = int(os.environ.get("API_ID", ""))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "fedd630ba4bd77044ee4e5a00e5300e6")
+API_HASH = os.environ.get("API_HASH", "")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8683411777:AAFAmPQHFVAxPep-e9QPixiwcUK17QS_XvQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("Eeeexbot")
 # ------------------X------------------------------
@@ -19,6 +19,6 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1717411093").split()))
 # ------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003549344180"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Bot_user:gfZcDelYJBwPda9A@cluster0.thbvcoa.mongodb.net/?appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003549344180"))
